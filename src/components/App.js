99e53f1d0e26f6aa03d4abe18fce9883543810ea
anitha-anii <anithachowdary355@gmail.com>
2023-07-18 15:00:41 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import '../styles/App.css';
-import Tabs from "./Tabs";
+import React, { useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+const Tab = ({ name, activeTab, setActiveTab }) => {
+  const isActive = activeTab === name;
+  const tabId = `${name}-Tab`; 
+
+  const handleClick = () => {
+    setActiveTab(name);
+  };
+
   return (
-    <div>
-      <h1>Tab Demo</h1>
-      <Tabs />
+    <div
+      className={`tab ${isActive ? "active" : ""}`}
+      id={tabId}
+      onClick={handleClick}
+      data-testid={`tab-${name}`}
+    >
+      {name}
     </div>
   );
 };
+
+const App = () => {
+  const [activeTab, setActiveTab] = useState("Home");
+
+  return (
+    <div className="tabs">
+      <Tab name="Home" activeTab={activeTab} setActiveTab={setActiveTab} />
+      <Tab name="About" activeTab={activeTab} setActiveTab={setActiveTab} />
+      <Tab name="Features" activeTab={activeTab} setActiveTab={setActiveTab} />
+      <div className="viewport">Pages Go Here</div>
+    </div>
+  );
+};
+
 
 export default App;
