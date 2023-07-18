b8eaa92cb9053c52780aff44039e6e9e996a9448
anitha-anii <anithachowdary355@gmail.com>
2023-07-18 15:15:21 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,15 @@
-import React, { useState } from "react";
-import "../styles/App.css";
+import React from 'react';
+import './App.css';
+import Tabs from './Tabs';
+import Viewport from './Viewport';
 
-const Tab = ({ name, activeTab, setActiveTab }) => {
-  const isActive = activeTab === name;
-  const tabId = `${name}-Tab`; 
-
-  const handleClick = () => {
-    setActiveTab(name);
-  };
-
+function App() {
   return (
-    <div
-      className={`tab ${isActive ? "active" : ""}`}
-      id={tabId}
-      onClick={handleClick}
-      data-testid={`tab-${name}`}
-    >
-      {name}
+    <div className="app">
+      <Tabs />
+      <Viewport />
     </div>
   );
-};
-
-const App = () => {
-  const [activeTab, setActiveTab] = useState("Home");
-
-  return (
-    <div className="tabs">
-      <Tab name="Home" activeTab={activeTab} setActiveTab={setActiveTab} />
-      <Tab name="About" activeTab={activeTab} setActiveTab={setActiveTab} />
-      <Tab name="Features" activeTab={activeTab} setActiveTab={setActiveTab} />
-      <div className="viewport">Pages Go Here</div>
-    </div>
-  );
-};
-
+}
 
 export default App;
---
+++ src/components/Viewport.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './Viewport.css'; 
+
+const Viewport = () => {
+  return (
+    <div className="viewport">
+      Pages Go Here
+    </div>
+  );
+};
+
+export default Viewport;
